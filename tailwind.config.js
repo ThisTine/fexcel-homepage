8d4d0d78becbc0b24b486ce1b0833de84e6ee8a5
I_AM_TINE <sutichok46@gmail.com>
2022-02-14 00:27:20 +0700

    add new path

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
+    "./src/**/*.html"
   ],
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily:{
