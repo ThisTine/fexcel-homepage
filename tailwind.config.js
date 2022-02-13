6193cb5af573ca1c7ef944c3b93bddb628abce3c
I_AM_TINE <sutichok46@gmail.com>
2022-02-14 00:29:43 +0700

    add path2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
     "./public/**/*.html",
     "./src/**/*.html"
   ],
