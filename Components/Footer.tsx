820372095f6e01ee35a3721284551fa560c94029
Rutthee Raywatkhunanon <rutthee9999@gmail.com>
2023-04-06 01:52:07 +0700

    update some registrarion page

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -9,6 +9,7 @@ const Footer:FC = ()=>{
             <h2 className="text-lg font-bold">Fexcel</h2>
             <Link href="/"><a className="hover:underline">Home</a></Link>
             <Link href="/plans"><a className="hover:underline">Pricing</a></Link>
+            <Link href="/register"><a className="hover:underline">Register</a></Link>
           </div>
           <div className="flex flex-col gap-3" >
             <h2 className="text-lg font-bold">Contact us</h2>
